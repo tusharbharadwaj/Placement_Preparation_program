98c0388ccee5994abde4bfb943290fcb17325454
Tushar Bharadwaj <tushar.bharadwaj23@gmail.com>
2023-07-03 12:29:48 +0530

    Day-1 array programs

---
+++ DAY-1/program1.java
@@ -0,0 +1,43 @@
+import java.util.Arrays;
+class setmatrixzeroes {
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {1,1,1,},
+                {1,0,1},
+                {1,1,1}
+        };
+        System.out.println(Arrays.toString(setZeroes(matrix)));
+    }
+    public int[] setZeroes(int[][] matrix) {
+        boolean fr = false, fc = false;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == 0) {
+                    if (i == 0) fr = true;
+                    if (j == 0) fc = true;
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
+                }
+            }
+        }
+        for (int i = 1; i < matrix.length; i++) {
+            for (int j = 1; j < matrix[0].length; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        if (fr) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+        if (fc) {
+            for (int i = 0; i < matrix.length; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+}
+
